1c0ebc0f0170aead5e44ba314fc31f1526f5a7ff
Delong Zhao <zhaodelong@gmail.com>
2015-10-13 10:49:44 -0500

    modify quesiton 3 with correct 0000 result, hex is half byte lol

--- sha1.py
+++ sha1.py
@@ -9,19 +9,23 @@ hex_hash = m.hexdigest()
 print(hex_hash)
 print(hex_hash[0])
 #print(str(m.digest()))
-print("block size: ", m.block_size)
-print("digest size: ", m.digest_size)
+#print("block size: ", m.block_size)
+#print("digest size: ", m.digest_size)
 
 ascii_all = ascii_letters + digits
-for i in ascii_all:
-	in_str = "113800046"
-	in_str = in_str + i
-	m.update(str(in_str))
-	hex_hash = m.hexdigest()
-	#print(in_str)
-	#print(hex_hash)
-	#print(hex_hash[0])
-	if (hex_hash[0] == "0"):
-		print(in_str)
-		print(hex_hash)
-		print(hex_hash[0])
+found = False
+while(not found):
+	for i in ascii_all:		
+		in_str = in_str + i
+		m.update(str(in_str))
+		hex_hash = m.hexdigest()
+		#print(in_str)
+		#print(hex_hash)
+		#print(hex_hash[0])
+		if (hex_hash[0] == "0" and hex_hash[1] == "0" and hex_hash[2] == "0" and hex_hash[3] == "0"):
+			print(in_str)
+			print(hex_hash)
+			found = True
+			break
+
+
