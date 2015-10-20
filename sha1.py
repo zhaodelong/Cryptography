e479d0f10e12cd2fa3b50f943cecf13ed1931043
Delong Zhao <zhaodelong@gmail.com>
2015-10-19 23:03:44 -0500

    fixed sha1 with permutations

--- sha1.py
+++ sha1.py
@@ -1,32 +1,27 @@
 import hashlib
 from string import *
+from itertools import *
 
-m = hashlib.sha1()
-in_str = "113800046"
-print(in_str)
-m.update(in_str)
-hex_hash = m.hexdigest()
-print(hex_hash)
-print(hex_hash[0])
-#print(str(m.digest()))
-#print("block size: ", m.block_size)
-#print("digest size: ", m.digest_size)
-
-ascii_all = ascii_letters + digits
 found = False
-while(not found):
-	for i in ascii_all:		
-		m.update(i)
-		in_str = in_str + i;
-		hex_hash = m.hexdigest()
-		#print(in_str)
-		#print(hex_hash)
-		#print(hex_hash[0])
+ascii_all = ascii_letters + digits
+for i in range(len(ascii_all)):
+	if found:
+		break
+	print("Elements for permutation " + str(i + 1))
+	permut = permutations(ascii_all, i + 1)
+	permut = list(map("".join, permut))
+	print("Number of permutations: " + str(len(permut)))
+	for x in permut:
+		in_str = "113800046"
+		in_str = in_str + x
+		m = hashlib.sha1()
+		m.update(in_str)
+		hex_hash = m.hexdigest()	
 		if (hex_hash[0] == "0" and hex_hash[1] == "0" and hex_hash[2] == "0" and hex_hash[3] == "0"):
 			print(in_str)
 			print(hex_hash)
 			found = True
 			break
-		
+		in_str = "113800046"
 
-
+print("Finished")
